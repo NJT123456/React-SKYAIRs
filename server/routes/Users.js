335f14501075b24f5c2ae85ab0b4a32b82c8b9d4
NJT123456 <jinjutha.njt@gmail.com>
2023-09-14 22:42:48 +0700

    client

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const router = express.Router()
-const {Users} = require('../models')
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
-router.post('/', async(req, res)=>{
-  const {username, password} = req.body
-  bcrypt.hash(password, 10).then(hash =>{
-    Users.create({
+router.post("/", async (req, res) => {
+  const { username, password, password1 } = req.body;
+
+  // ตรวจสอบว่า username นี้มีอยู่แล้ว
+  const existingUser = await Users.findOne({ where: { username: username } });
+
+  if (existingUser) {
+    return res.json({ error: "Username already exists" });
+  }
+
+  // ตรวจสอบว่ารหัสผ่านและรหัสผ่านยืนยันตรงกัน
+  if (password !== password1) {
+    return res.json({ error: "Passwords do not match" });
+  }
+
+  // ถ้ารหัสผ่านตรงกัน
+  bcrypt.hash(password, 10).then(async (hash) => {
+    await Users.create({
       username: username,
-      password: hash
-    })
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
+});
 
-    res.json('SUCCESS')
-  })
-})
-
-module.exports = router
+module.exports = router;
